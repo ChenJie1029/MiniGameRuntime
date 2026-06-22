db692d1504c73b3f126899ad97e1808598199c2a
ChenJie <1439475057@qq.com>
2026-06-22 22:08:39 +0800

    Add Vec2 math structure

--- MiniGameRuntime/src/main.cpp
+++ MiniGameRuntime/src/main.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+struct Vec2 {
+    float x;
+    float y;
+
+    Vec2();
+    Vec2(float xValue, float yValue);
+
+    Vec2 Add(const Vec2& other) const;
+    Vec2 Subtract(const Vec2& other) const;
+    Vec2 Multiply(float scalar) const;
+};
+
 int main() {
     cout << "MiniGameRuntime started." << endl;
 
