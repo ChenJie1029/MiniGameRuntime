3a0aec63bd3fb582eeba04bcb0dd9bb3670b6854
ChenJie <1439475057@qq.com>
2026-06-22 22:52:31 +0800

    6.22

---
+++ MiniGameRuntime/src/Vec2.h
@@ -0,0 +1,14 @@
+#pragma once
+
+struct Vec2 {
+    float x;
+    float y;
+
+    Vec2(); //默认构造函数
+    Vec2(float xValue, float yValue); //带参构造函数 用传入的 xValue 和 yValue 来初始化 x 和 y
+
+    //成员函数（方法）
+    Vec2 Add(const Vec2& other) const; //向量加法：将当前向量与 other 向量相加，返回一个新的 Vec2 对象
+    Vec2 Subtract(const Vec2& other) const;
+    Vec2 Multiply(float scalar) const;
+};
--- MiniGameRuntime/src/main.cpp
+++ MiniGameRuntime/src/main.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
+#include "Vec2.h"
 
 using namespace std;
 
-struct Vec2 {
-    float x;
-    float y;
-
-    Vec2();
-    Vec2(float xValue, float yValue);
-
-    Vec2 Add(const Vec2& other) const;
-    Vec2 Subtract(const Vec2& other) const;
-    Vec2 Multiply(float scalar) const;
-};
-
 int main() {
     cout << "MiniGameRuntime started." << endl;
+
+    //调用带参构造函数创建a, b
+    Vec2 a(1.0f, 2.0f);
+    Vec2 b(3.0f, 4.0f);
+
+    //调用带参构造函数创建c, 但是c又调用了成员方法
+    Vec2 c = a.Add(b);
+
+    cout << "c.x：" << c.x << " " << "c.y：" << c.y << endl;
 
     return 0;
 }
