10bbd9cfc3fad01ea73dad3d1c7a4ba0936e7733
ChenJie <1439475057@qq.com>
2026-07-17 21:29:07 +0800

    7.17 2

--- MiniGameRuntime/include/Core/ObjectPool.h
+++ MiniGameRuntime/include/Core/ObjectPool.h
@@ -13,5 +13,18 @@ namespace Hazel {
 		virtual void Reset() = 0;
 	};
 
-	//
+	template<typename T>
+	class ObjectPool {
+		//编译时，如果编译不通过报错
+		static_assert(std::is_base_of<IPoolable, T>::value, "Template type T must inherit from IPoolable");
+
+	public:
+		explicit ObjectPool(size_t initialCapacity) { // explicit 显示关键字
+			m_pool.resize(initialCapacity);
+			m_activeCount = 0;
+			std::cout << "[ObjectPool] Initialized with capacity: " << initialCapacity << std::endl;
+		}
+
+		~ObjectPool() = default;
+	};
 }
