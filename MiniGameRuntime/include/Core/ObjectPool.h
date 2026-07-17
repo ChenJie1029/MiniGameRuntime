20fd4bf0d6d9c2c89ab381149994acccffe80e7c
ChenJie <1439475057@qq.com>
2026-07-17 18:20:05 +0800

    7.17 ObjectPool 1

---
+++ MiniGameRuntime/include/Core/ObjectPool.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <stdexcept>
+
+namespace Hazel {
+	class IPoolable {
+	public:
+		virtual ~IPoolable() = default;
+		virtual void Init() = 0;
+		virtual void Reset() = 0;
+	};
+
+	
+}
