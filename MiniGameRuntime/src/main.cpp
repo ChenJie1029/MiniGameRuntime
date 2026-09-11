0b5e46e55e7f020e8fec028b34e0c2b47fcac010
ChenJie <1439475057@qq.com>
2026-09-11 11:38:36 +0800

    9.11 4.1.7

--- MiniGameRuntime/src/main.cpp
+++ MiniGameRuntime/src/main.cpp
@@ -19,6 +19,15 @@ int main()
 
     entityManager.DestroyEntity(enemy);
 
+    Entity third = entityManager.CreateEntity();
+
+    std::cout << "Third ID: " << third << std::endl;
+
+    std::cout << "Third alive: " << entityManager.IsAlive(third) << std::endl;
+
+    std::cout << "Living count after third: " << entityManager.GetEntityCount() << std::endl;
+
+
     std::cout << "Enemy alive: " << entityManager.IsAlive(enemy) << std::endl;
 
     std::cout << "Living count: " << entityManager.GetEntityCount() << std::endl;
