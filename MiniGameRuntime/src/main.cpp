97e9de49b339f792c32b54212a43f9a883e3d91b
ChenJie <1439475057@qq.com>
2026-09-21 10:34:59 +0800

    9.21 5.1.1

--- MiniGameRuntime/src/main.cpp
+++ MiniGameRuntime/src/main.cpp
@@ -1,87 +1,43 @@
 #include <iostream>
 
-#include "Core/World.h"
-#include "Systems/CollisionSystem.h"
+#include "Math/Vec2.h"
 
-bool HandleInput(World& world, Entity player) {
-    while (true) {
-        std::cout << "Move (W/A/S/D, Q to quit): ";
-
-        char input;
-
-        if (!(std::cin >> input)) {
-            std::cout << "Input error!" << std::endl;
-            return false;
-        }
-
-        VelocityComponent& velocity = world.GetVelocity(player);
-
-        switch (input) {
-        case 'W':
-        case 'w':
-            velocity.velocity = Vec2{ 0.0f, 2.0f };
-            return true;
-        case 'A':
-        case 'a':
-            velocity.velocity = Vec2{ -2.0f, 0.0f };
-            return true;
-        case 'S':
-        case 's':
-            velocity.velocity = Vec2{0.0f, -2.0f};
-            return true;
-        case 'D':
-        case 'd':
-            velocity.velocity = Vec2{ 2.0f, 0.0f };
-            return true;
-        case 'Q':
-        case 'q':
-            return false;
-        default:
-            std::cout << "Re-enter!" << std::endl;
-            continue;
-        }
-    }
+Vec2 CalculateDisplacement(const Vec2& velocity, float deltaTime) {
+    return velocity * deltaTime;
 }
 
-void PrintPosition(World& world, Entity entity) {
-    const TransformComponent& transform = world.GetTransform(entity);
-
-    std::cout
-        << "Player position: ("
-        << transform.position.x << ", "
-        << transform.position.y << ")"
-        << std::endl;
+void Move(Vec2& position, const Vec2& velocity, float deltaTime) {
+    Vec2 displacement = CalculateDisplacement(velocity, deltaTime);
+    position = position + displacement;
 }
 
-void RunSandbox() {
-    World world;
-    CollisionSystem collisionSystem;
-
-    Entity boxA = world.CreateEntity();
-    world.AddTransform(boxA, TransformComponent{ Vec2{0.0f, 0.0f} });
-    world.AddVelocity(boxA, VelocityComponent{ Vec2{0.0f, 0.0f} });
-    world.AddAABB(boxA, AABBComponent{ 1.0f, 1.0f });
-
-    Entity boxB = world.CreateEntity();
-    world.AddTransform(boxB, TransformComponent{ Vec2{5.0f, 0.0f} });
-    world.AddAABB(boxB, AABBComponent{ 1.0f, 1.0f });
-
-    constexpr float deltaTime = 0.5f;
-
-    while (HandleInput(world, boxA)) {
-        world.Update(deltaTime);
-        collisionSystem.Update(world);
-        PrintPosition(world, boxA);
-    }
-
-    std::cout << "Sandbox closed." << std::endl;
+void PrintVec2(const Vec2& vector) {
+    std::cout << "(" << vector.x << ", " << vector.y << ")" << std::endl;
 }
 
 int main()
 {
-    std::cout << "MiniGameRuntime Sandbox" << std::endl;
+    // ======================================================================
+    std::cout << "======================================================================" << std::endl;
 
-    RunSandbox();
+    Vec2 position{ 10.0f, 5.0f };
+    Vec2 velocity{ 4.0f, -2.0f };
+
+    constexpr float deltaTime = 0.5f;
+
+    std::cout << "Initial position: ";
+    PrintVec2(position);
+
+    std::cout << "Velocity: ";
+    PrintVec2(velocity);
+
+    std::cout << "Delta time: " << deltaTime << std::endl;
+
+    for (int frame = 1; frame <= 3; frame++) {
+        Move(position, velocity, deltaTime);
+        std::cout << "Frame: " << frame << std::endl;
+        PrintVec2(position);
+    }
 
     return 0;
 }
